92da83b7e11108e573a48c9bb41be2a899d012bf
Russell Propert <russellpropert@gmail.com>
2021-09-24 15:48:59 -0400

    Adding clearer directions for mobile screens.

--- components/Home.js
+++ components/Home.js
@@ -16,7 +16,7 @@ function Home() {
       text={context.currentUser !== null ?
          `Your current balance is $${context.data.users[userIndex].balance}. Click on Deposit to put money in your account and Withdraw
          to take some out. You can see all of the site's users and transactions in All Data.`:
-        `This is page demonstrates a single page application created with React. Click on Create Account to create your account 
+        `This is page demonstrates a single page application created with React. Click on Create Account in the menu to create your account 
          and then log in. You’ll be given options to Deposit and Withdraw money and to view All Data to see all of the site's 
          users and transactions.`
       }
